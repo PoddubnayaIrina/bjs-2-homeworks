017bd01501237589537ba9116708bbef85b1f158
Poddubnaia Irina <vahtaerzz@gmail.com>
2022-12-18 22:34:14 +0300

    task solved

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.every((element, index) => element === arr2[index]) && arr1.length === arr2.length;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users
+  .filter(user => user.gender === gender)
+  .map(person => person.age)
+  .reduce((acc, userAge, index, array) => {
+    acc += userAge;
+    if (index === array.length - 1) {
+    return acc / array.length;
+      } 
+    return acc;
+  }, 0);
 }
